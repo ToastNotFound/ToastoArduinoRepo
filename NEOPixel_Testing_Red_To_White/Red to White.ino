bcc86b0a2917e178442ad369ee43f36cf886792b
Toast <sammie.tech567@gmail.com>
2021-04-21 11:29:02 +0100

    Added tips and descriptions

--- NEOPixel_Testing_Red_To_White/Red to White.ino
+++ NEOPixel_Testing_Red_To_White/Red to White.ino
@@ -1,51 +1,40 @@
-#include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel.h> //Import the Adafruit Library to use the LEDS, can be found online or in the 'board docs' folder in my github repository.
 #ifdef __AVR__
-  #include <avr/power.h>
+  #include <avr/power.h> //Power to the LEDS.
 #endif
 
-#define Button 2
-#define PIN1 12
-#define PIN2 13
-#define LED_COUNT 2
-Adafruit_NeoPixel pixel_1(LED_COUNT, PIN1, NEO_RGB + NEO_KHZ800);
-Adafruit_NeoPixel pixel_2(LED_COUNT, PIN2, NEO_RGB + NEO_KHZ800);
+#define Button 2  
+#define PIN1 12 //LED 1.
+#define PIN2 13 //LED 2.
+#define LED_COUNT 2 //Tells the board that we will be using the 2 LEDs on the board ONLY.
+Adafruit_NeoPixel pixel_1(LED_COUNT, PIN1, NEO_RGB + NEO_KHZ800); //Configures the first LED within Adafruit.
+Adafruit_NeoPixel pixel_2(LED_COUNT, PIN2, NEO_RGB + NEO_KHZ800); //Configures the second LED within Adafruit.
 void setup() {
-  // put your setup code here, to run once:
 
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
 
-  pinMode(11,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(9,OUTPUT);
-  pinMode(8,OUTPUT);
-  pinMode(7,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(2,INPUT_PULLUP);
-  Serial.begin(9600);
-  pixel_1.begin();
-  pixel_2.begin();
+  pinMode(2,INPUT_PULLUP); //Tells the board to treat pin 2 as a switch.
+  Serial.begin(9600); //Starts the serial for debug, optional.
+  pixel_1.begin(); //Initialises the first LED, this will blank and turn off the LED ready for the instructions in the loop() section.
+  pixel_2.begin(); //Initialises the second LED, this will blank and turn off the LED ready for the instructions in the loop() section.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
 
   if(digitalRead(Button)==LOW){
-    pixel_1.setPixelColor(0, pixel_1.Color(255, 255, 255));
-    pixel_2.setPixelColor(0, pixel_2.Color(255, 255, 255));
-    pixel_1.show();
-    pixel_2.show();
+    pixel_1.setPixelColor(0, pixel_1.Color(255, 255, 255)); //White RGB Value
+    pixel_2.setPixelColor(0, pixel_2.Color(255, 255, 255)); //White RGB Value
+    pixel_1.show(); //Displays the colour previously defined
+    pixel_2.show(); //Displays the colour previously defined
     delay(1);
     Serial.print("1111");
   } else if (digitalRead(Button)==HIGH){
-    pixel_1.setPixelColor(0, pixel_1.Color(0, 55, 0));
-    pixel_2.setPixelColor(0, pixel_2.Color(0, 55, 0));
-    pixel_1.show();
-    pixel_2.show();
+    pixel_1.setPixelColor(0, pixel_1.Color(0, 55, 0)); //Red RGB Value
+    pixel_2.setPixelColor(0, pixel_2.Color(0, 55, 0)); //Red RGB Value
+    pixel_1.show(); //Displays the colour previously defined
+    pixel_2.show(); //Displays the colour previously defined
     delay(1);
     Serial.print("2222");
   }

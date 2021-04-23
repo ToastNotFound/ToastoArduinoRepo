d9e75b784da06953ba35a6ae268dee1394d18f8c
Toast <sammie.tech567@gmail.com>
2021-04-23 13:44:54 +0100

    Added new sketch - Strobe (1.3)

--- Sketches - NEOPixel LEDs/NEOPixel_White_Strobe/NEOPixel_White_Strobe.ino
+++ Sketches - NEOPixel LEDs/NEOPixel_White_Strobe/NEOPixel_White_Strobe.ino
@@ -2,9 +2,7 @@
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
-
-#define Button 2
-#define PIN1 13
+#define PIN1 12
 #define PIN2 13
 #define LED_COUNT 2
 Adafruit_NeoPixel pixel_1(LED_COUNT, PIN1, NEO_RGB + NEO_KHZ800);
@@ -14,8 +12,6 @@ void setup() {
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
-
-  pinMode(2,INPUT_PULLUP);
   pixel_1.begin();
   pixel_2.begin();
 }
@@ -31,4 +27,5 @@ void loop()
     pixel_2.setPixelColor(0, pixel_2.Color(255, 255, 255));
     pixel_1.show();
     pixel_2.show();
+    delay(1000)
 }
